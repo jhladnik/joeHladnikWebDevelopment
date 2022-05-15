18e67a6283e716f745f46de81ae32effb14570bf
Joe Hladnik <jhladnik20@gmail.com>
2022-05-15 14:10:06 -0500

    Add js, css, scss, html,  and assets jhwd web app build

---
+++ Core Portfolio/assets/js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+
+	
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+})(jQuery);
